36c85557f68593bdb6e1ec326ca43455f090cd5c
Dennis Möhlmann <dmo479@gmail.com>
2018-02-13 23:41:16 +0100

    resolve factions on load

--- src/def/entity.rs
+++ src/def/entity.rs
@@ -45,8 +45,37 @@ fn default_scale() -> f32 {
 
 pub fn parse_entities() -> Result<EntityDef, Error> {
     let factions: Vec<String> = parse_file("res/def/faction.yaml", |_, _, _| {}).unwrap();
-    parse_dir("res/def/entity/", &[ "yaml" ], |ref v, k, p| {
-        println!("{:?}.{:?}: {:?} ", p, k, v);
+    parse_dir("res/def/entity/", &[ "yaml" ], |v, k, p| {
+        use serde_yaml::Value::*;
+        //println!("{:?}.{:?}: {:?} ", p, k, v);
+        
+        if p.is_some() && p.unwrap().as_str() == Some(&"bounding") {
+
+            if k.is_some() && k.unwrap().as_str() == Some(&"faction") {
+                
+                if let Some(index) = factions.iter().position(|&ref x| x == v.as_str().unwrap()) {
+                    *v = Number(index.into());
+                }
+            }
+
+        }
+/*
+        if let Some(&String(ref p)) = p {
+            if p == "bounding" {
+
+                if let Some(&mut String(ref mut k)) = k {
+                    if k == "faction" {
+
+                        if let Some(index) = factions.iter().position(|&ref x| x == v.as_str().unwrap()) {
+                            *v = Number((index as u32).into());
+                        }
+
+                    }
+                }
+
+            }
+        }
+*/        
     })
 }
 
